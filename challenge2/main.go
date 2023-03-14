254bf6ac599d535030819a6fded6bc2835d9fa5e
christhopermarcelino <christhopermarcelino@gmail.com>
2023-03-14 12:44:44 +0700

    add challenge2

---
+++ challenge2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("Nilai i =", i)
+	}
+
+	rus := "\u0421\u0410\u0428\u0410\u0420\u0412\u041E"
+
+	for j := 0; j < 11; j++ {
+		if j == 5 {
+			for index, value := range rus {
+				fmt.Printf("character %#U starts at byte position %d\n", value, index)
+			}
+			continue
+		}
+		fmt.Println("Nilai j =", j)
+	}
+}
